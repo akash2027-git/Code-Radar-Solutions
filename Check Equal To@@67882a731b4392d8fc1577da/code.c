d562577a5ee29fdd12bc398cf04e8b1bf6d7490e
akash2027-git <9090akashkum@gmail.com>
2025-01-21 18:15:09 +0530

    Solution to Check Equal To

--- Check Equal To@@67882a731b4392d8fc1577da/code.c
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -1,1 +1,12 @@
-Loading Code...
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a, &b);
+    if(a == b) {
+        printf("True");
+    } else {
+        printf("False");
+    }
+    return 0;
+}
