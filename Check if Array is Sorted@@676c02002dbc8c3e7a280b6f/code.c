6fa5942fb5a935ef5b8c0c3fdf6ecb54d3533bf0
AKASH KUMAR <9090akashkum@gmail.com>
2025-04-01 00:08:02 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,37 +1,57 @@
-#include <stdio.h>
-#include <stdbool.h>
+// #include <stdio.h>
+// #include <stdbool.h>
 
-// Function to check if the array is sorted in ascending order
-bool isSorted(int arr[], int n) {
-    for(int i = 0; i < n-1; i++) {
+// // Function to check if the array is sorted in ascending order
+// bool isSorted(int arr[], int n) {
+//     for(int i = 0; i < n-1; i++) {
+//         if(arr[i] > arr[i+1]) {
+//             return false; // Returns false if any element is greater than the next one.
+//         }
+//     }
+//     return true; // Returns true if all elements are in order
+// }
+
+// int main() {
+//     int n;
+//     scanf("%d ", &n);
+//     // Declare an Array of size n
+//     int arr[n];
+
+//     // Prompt the user to enter the elements of the Array 
+//     for(int i = 0; i < n; i++) {
+//         scanf("%d ", &arr[i]);
+//     }
+
+//     // Check is the Array is sorted
+//     if(isSorted(arr, n)) {
+//         printf("Sorted\n");
+//     } else {
+//         printf("Not Sorted\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+bool isSroted(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
         if(arr[i] > arr[i+1]) {
-            return false; // Returns false if any element is greater than the next one.
+            return false;
         }
     }
-    return true; // Returns true if all elements are in order
+    return true;
 }
 
 int main() {
     int n;
-    scanf("%d ", &n);
-    // Declare an Array of size n
+    scanf("%d", &n);
     int arr[n];
-
-    // Prompt the user to enter the elements of the Array 
     for(int i = 0; i < n; i++) {
-        scanf("%d ", &arr[i]);
+        scanf("%d", &arr[i]);
     }
-
-    // Check is the Array is sorted
-    if(isSorted(arr, n)) {
-        printf("Sorted\n");
+    if isSorted(arr, n) {
+        printf("Sorted");
     } else {
-        printf("Not Sorted\n");
+        printf("Not Sorted");
     }
-    return 0;
 }
-
-
-
-
-
